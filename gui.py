dd2fcc1a1f78a5e7271ccb7c647553ba0932f5a9
Varsha Tewatia <varshatewatia11@gmail.com>
2026-07-10 14:49:52 +0530

    Added frames in the UI

--- gui.py
+++ gui.py
@@ -7,37 +7,50 @@ ctk.set_default_color_theme("blue")
 
 app = ctk.CTk()
 app.title("Password Security Analyzer")
-app.geometry("1000x900")
+app.geometry("1200x1000")
 
 title = ctk.CTkLabel(
     app,
     text="🔐 Password Security Analyzer",
-    font=("Poppins", 36, "bold"),
-    text_color="white"
+    font=("Poppins",42,"bold"),
+    text_color="#4CC9F0"
+)
+title.pack(pady=20)
+
+main_frame = ctk.CTkFrame(
+    app,
+    width=760,
+    height=500,
+    corner_radius=25,
+    fg_color="#202235",
+    border_width=2,
+    border_color="#00E5FF"
 )
 
-title.pack(pady=20)
+main_frame.pack(pady=20)
+main_frame.pack_propagate(False)
+
 name_entry = ctk.CTkEntry(
-    app,
+    main_frame,
     width=600,
     height=45,
-    placeholder_text="Enter Your Name"
+    placeholder_text="👤 Full Name"
 )
 name_entry.pack(pady=15)
 
 email_entry = ctk.CTkEntry(
-    app,
+    main_frame,
     width=600,
     height=45,
-    placeholder_text="Enter Your Email"
+    placeholder_text="📧 Email Address"
 )
 email_entry.pack(pady=15)
 
 password_entry = ctk.CTkEntry(
-    app,
+    main_frame,
     width=600,
     height=45,
-    placeholder_text="Enter Password",
+    placeholder_text="🔑 Enter Password",
     show="*"
 )
 password_entry.pack(pady=15)
@@ -50,7 +63,7 @@ def toggle_password():
         password_entry.configure(show="*")
 
 show_checkbox = ctk.CTkCheckBox(
-    app,
+    main_frame,
     text="Show Password",
     variable=show_password,
     command=toggle_password
@@ -107,21 +120,21 @@ def save_report():
                 file.write(report)
 
 analyze_btn = ctk.CTkButton(
-    app,
+    main_frame,
     text="Analyze Password",
     command=analyze_clicked,
-    width=240,
-    height=45,
-    corner_radius=12,
+    width=260,
+    height=48,
+    corner_radius=15,
     font=("Poppins", 16,"bold"),
-    fg_color="#00B4D8",
-    hover_color="#0096C7"
+    fg_color="#0077B6",
+    hover_color="#023E8A"
 )
 
 analyze_btn.pack(pady=10)
 
 progress = ctk.CTkProgressBar(
-    app,
+    main_frame,
      width=750,
      height=18
 )
@@ -129,46 +142,63 @@ progress.set(0)
 progress.pack(pady=10)
 
 strength_label = ctk.CTkLabel(
-    app,
-    text="Password Strength: Not Checked",
+    main_frame,
+    text="📊 Password Strength: Not Checked",
     font=("Poppins", 16, "bold")
 )
 strength_label.pack(pady=5)
 
 save_btn = ctk.CTkButton(
-    app,
+    main_frame,
     text="💾 Save Report",
     command=save_report,
-    width=240,
-    height=45,
-    corner_radius=12,
+    width=260,
+    height=48,
+    corner_radius=15,
     font=("Poppins", 16,"bold"),
-    fg_color="#00B4D8",
-    hover_color="#0096C7"
+    fg_color="#0077B6",
+    hover_color="#023E8A"
 )
 save_btn.pack(pady=5)
 
-result_heading = ctk.CTkLabel(
+report_frame = ctk.CTkFrame(
     app,
-    text="Analysis Report:",
+    width=920,
+    height=420,
+    corner_radius=25,
+    fg_color="#202235",
+    border_width=2,
+    border_color="#00E5FF"
+)
+
+report_frame.pack(pady=15)
+report_frame.pack_propagate(False)
+
+result_heading = ctk.CTkLabel(
+    report_frame,
+    text="📋 Analysis Report:",
     font=("Poppins", 18, "bold"),
     text_color="white"
 )
-result_heading.pack(pady=15, padx=10)
+result_heading.pack(pady=(15,10))
 
 result = ctk.CTkTextbox(
-    app,
+    report_frame,
     width=850,
-    height=380,
+    height=280,
     font=("Poppins", 15),
+    corner_radius=15,
+    border_width=2,
+    border_color="#00E5FF",
 )
-result.pack(pady=20)
+result.pack(pady=10)
+
 footer = ctk.CTkLabel(
     app,
-    text="Developed by Varsha Tewatia | © 2026 All Rights Reserved",
+    text="🔒 Developed by Varsha Tewatia • Python • CustomTkinter • 2026",
     font=("Poppins", 12),
-    text_color="gray"
+    text_color="#8A8A8A"
 )
-footer.pack(pady=10)
+footer.pack(pady=(5,10))
 
 app.mainloop()
